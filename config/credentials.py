b84573501724f80c9c7b47bf80c84e7aa5528abd
jherrerotardon <jherrero.tardon@gmail.com>
2019-12-01 23:12:52 +0100

    Extract tweets and define sql structure.

--- config/credentials.py
+++
@@ -1,8 +0,0 @@
-consumer_keys = {
-    'consumer_key': '',
-    'consumer_secret': '',
-}
-access_tokens = {
-    'access_token_key': '',
-    'access_token_secret': '',
-}
--- src/test.py
+++ src/test.py
@@ -12,17 +12,29 @@ if __name__ == '__main__':
     from config.credentials import access_tokens, consumer_keys
 
     auth = tweepy.OAuthHandler(**consumer_keys)
-    auth.set_access_token(**access_tokens)
+    auth.set_access_token(access_tokens['access_token_key'], access_tokens['access_token_secret'])
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
-    #####United Airlines
+
     # Open/Create a file to append data
-    csvFile = open('ua.csv', 'a')
-    # Use csv Writer
+    csvFile = open('tweets.csv', 'a')
     csvWriter = csv.writer(csvFile)
 
-    for tweet in tweepy.Cursor(api.search, q="#PSOE", count=100,
-                               lang="en",
-                               since="2019-10-01").items():
+    # SQL Table columns.
+    # -----> tweet_id (using to avoid duplicated tweets)
+    # -----> created_at (creation date of tweet)
+    # -----> text (tweet content)
+    # -----> is_verified (user.verified field to know if tweet ()
+    # -----> username (user.name field who wrote tweet)
+    # -----> political_party (to join tweet with any political party)
+
+    for tweet in tweepy.Cursor(
+            api.search,
+            q="%28salamanca%20OR%20segovia%29 -filter:retweets",
+            count=100,
+            lang="es",
+            since="2019-11-30"
+
+    ).items():
         print(tweet.created_at, tweet.text)
         csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
